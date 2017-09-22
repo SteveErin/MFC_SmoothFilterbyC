0ab433877e9396ad3288e9a715e3ff293792204b
SteveSu <st872530917@gmail.com>
2017-09-22 18:52:09 +0800

    Add files via upload

---
+++ TEST/TEST/TEST/TESTDlg.h
@@ -0,0 +1,36 @@
+
+// TESTDlg.h : 標頭檔
+//
+
+#pragma once
+
+
+// CTESTDlg 對話方塊
+class CTESTDlg : public CDialogEx
+{
+// 建構
+public:
+	CTESTDlg(CWnd* pParent = NULL);	// 標準建構函式
+
+// 對話方塊資料
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_TEST_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支援
+
+
+// 程式碼實作
+protected:
+	HICON m_hIcon;
+
+	// 產生的訊息對應函式
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedOpen();
+};
